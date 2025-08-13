2cb859fb983a7fa39047ccdbb51459d2991bf646
nishant667 <nishant.patil@wohlig.com>
2025-08-13 12:41:40 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -21,7 +21,7 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: "https://ecommerce-backend-rdtv.onrender.com/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     // Check if user already exists
