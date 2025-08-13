aa91166ecdd1308814a5d3657802c88f6ae428c0
nishant667 <nishant.patil@wohlig.com>
2025-08-13 12:57:55 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,24 +4,18 @@ const authController = require('../controllers/authController');
 const passport = require('passport');
 
 // Registration routes
-router.get('/register', (req, res) => {
-  res.render('register');
-});
+router.get('/register', (req, res) => res.render('register'));
 router.post('/register', authController.registerUser);
 
 // Login routes
-router.get('/login', (req, res) => {
-  res.render('login');
-});
+router.get('/login', (req, res) => res.render('login'));
 router.post('/login', authController.loginUser);
 
 // Google OAuth routes
 router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-
-router.get('/auth/google/callback', 
+router.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
   (req, res) => {
-    // Set session variables for your app
     req.session.userId = req.user._id;
     req.session.username = req.user.username;
     res.redirect('/');
@@ -37,4 +31,4 @@ router.get('/profile', authController.getProfile);
 // Contact Us route
 router.post('/contact', require('../controllers/contactController').handleContact);
 
-module.exports = router; 
+module.exports = router;
